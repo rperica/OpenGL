58ccc11327fa73f59a7a7365cc82929759d98f90
rperica <zmaj@zmaj.com>
2020-01-16 16:01:55 +0100

    Created OpenGL debuger

--- OpenGL/src/Renderer.cpp
+++ OpenGL/src/Renderer.cpp
@@ -1,5 +1,23 @@
 #include "Renderer.h"
 
+#include <iostream>
+
+void GLClearError()
+{
+	while (glGetError() != GL_NO_ERROR);
+}
+
+bool GLLogCall(const char* function, const char* file, int line)
+{
+	while (GLenum error = glGetError())
+	{
+		std::cout << "[OpenGL Error](" << error << "): " << function << " " << file
+			<< ": " << line << std::endl;
+		return false;
+	}
+	return true;
+}
+
 void Renderer::Clear()
 {
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
--- OpenGL/src/Renderer.h
+++ OpenGL/src/Renderer.h
@@ -5,6 +5,14 @@
 #include "IndexBuffer.h"
 #include "Shader.h"
 
+#define ASSERT(x) if(!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+		x;\
+		ASSERT(GLLogCall(#x,__FILE__,__LINE__))
+
+void GLClearError();
+bool GLLogCall(const char* function, const char* file, int line);
+
 class Renderer
 {
 public:
