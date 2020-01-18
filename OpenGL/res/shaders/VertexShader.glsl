d9a7078a749000e64c83c4e0ce880d003a1ada8b
rperica <zmaj@zmaj.com>
2020-01-18 17:19:19 +0100

    Added MVP uniform

--- OpenGL/res/shaders/VertexShader.glsl
+++ OpenGL/res/shaders/VertexShader.glsl
@@ -3,10 +3,11 @@ layout (location=0) in vec3 positions;
 layout (location=1) in vec2 aTexCor;
 
 out vec2 TexCor;
+uniform mat4 u_MVP;
 
 void main()
 {
-	gl_Position=vec4(positions,1.0f);
+	gl_Position=u_MVP*vec4(positions,1.0f);
 	TexCor=aTexCor;
 }
 
