031e7ea30e4226c37e9c39548b5d2f92d7cfd1ea
rperica <zmaj@zmaj.com>
2020-02-02 18:08:06 +0100

    Created camera class

---
+++ OpenGL/src/Camera.cpp
@@ -0,0 +1,68 @@
+#include "Camera.h"
+
+Camera::Camera(glm::vec3 position, glm::vec3 worldUp, float yaw, float pitch)
+	:m_Position(position),m_Direction(0.0f,0.0f,-1.0f),m_Up(worldUp),
+	 m_sensivity(SENSIVITY),m_speed(SPEED),m_zoom(ZOOM),
+	 m_yaw(yaw),m_pitch(pitch),m_WorldUp(worldUp)
+{
+	UpdateCamera();
+}
+
+Camera::~Camera()
+{
+	//...
+}
+
+glm::mat4 Camera::View() const
+{
+	return glm::lookAt(m_Position,m_Position+m_Direction,m_Up);
+}
+
+void Camera::UpdateCamera()
+{
+	glm::vec3 direction;
+	direction.x = cos(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
+	direction.y = sin(glm::radians(m_pitch));
+	direction.z = sin(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
+
+	m_Direction = glm::normalize(direction);
+	m_Right = glm::cross(m_Direction, m_WorldUp);
+	m_Up = glm::cross(m_Right, m_Direction);
+}
+
+void Camera::KeyboardInput(cameraMovement moveDirection,float deltaTime)
+{
+	float velocity = m_speed * deltaTime;
+	if (moveDirection == FORWARD)
+		m_Position += m_Direction * velocity;
+	else if (moveDirection == BACKWARD)
+		m_Position -= m_Direction * velocity;
+	else if (moveDirection == LEFT)
+		m_Position += m_Right * velocity;
+	else if (moveDirection == RIGHT)
+		m_Position -= m_Right * velocity;
+}
+
+void Camera::MouseProcess(float xoffset, float yoffset, bool constrainPitch)
+{
+	xoffset *= m_sensivity;
+	yoffset *= m_sensivity;
+
+	m_yaw += xoffset;
+	m_pitch += yoffset;
+
+	if (constrainPitch)
+	{
+		if (m_pitch > 89.0f)
+			m_pitch = 89.0f;
+		if (m_pitch < -89.0f)
+			m_pitch = -89.0f;
+	}
+
+	UpdateCamera();
+}
+
+void Camera::ScrollProcess()
+{
+	//In development...
+}
---
+++ OpenGL/src/Camera.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+enum cameraMovement
+{
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float ZOOM = 45.0f;
+const float SPEED = 2.5f;
+const float SENSIVITY = 0.1f;
+
+class Camera
+{
+private:
+	glm::vec3 m_Position;
+	glm::vec3 m_Direction;
+	glm::vec3 m_Right;
+	glm::vec3 m_Up;
+	glm::vec3 m_WorldUp;
+
+	float m_yaw;
+	float m_pitch;
+	float m_speed;
+	float m_sensivity;
+	float m_zoom;
+
+	void UpdateCamera();
+
+public:
+	Camera(glm::vec3 position= glm::vec3(0.0f, 0.0f, 0.0f),glm::vec3 worldUp=glm::vec3(0.0f,1.0f,0.0f),float yaw=YAW,float pitch=PITCH);
+	~Camera();
+
+	glm::mat4 View() const;
+	
+	void KeyboardInput(cameraMovement moveDirection,float deltaTime);
+	void MouseProcess(float xoffset,float yoffset,bool constrainPitch=true);
+	void ScrollProcess();
+};
