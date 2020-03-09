a8d32fc92f554ecc7f10a029f162fd423c66c52b
rperica <zmaj@zmaj.com>
2020-03-09 19:59:47 +0100

    Implemented Phong lighting model

--- OpenGL/res/shaders/FragmentShader.glsl
+++ OpenGL/res/shaders/FragmentShader.glsl
@@ -1,12 +1,52 @@
 #version 330 core
 
 layout(location=0) out vec4 color;
+
+in vec3 Normal;
+in vec3 FragPos;
 in vec2 TexCor;
-uniform sampler2D u_Texture;
+
+struct Material
+{
+	sampler2D diffuse;
+	sampler2D specular;
+	float shininess;
+};
+
+struct Light 
+{
+	vec3 position;
+	vec3 direction;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+
+};
+
+uniform vec3 view;
+uniform vec3 lightPos;
+
+uniform Material material;
+uniform Light light;
 
 void main()
 {
-	vec4 texColor=texture(u_Texture,TexCor);
-	color=texColor;
+	
+	vec3 ambient=light.ambient*texture(material.diffuse,TexCor).rgb;
+
+	vec3 norm=normalize(Normal);
+	vec3 lightDir=normalize(light.position-FragPos);
+	float diff=max(dot(norm,lightDir),0.0f);
+	vec3 diffuse=light.diffuse*diff*texture(material.diffuse,TexCor).rgb;
+
+	float specularStrength=0.5f;
+	vec3 viewDir=normalize(view-FragPos);
+	vec3 reflectDir=reflect(-lightDir,norm);
+	float spec=pow(max(dot(viewDir,reflectDir),0.0),material.shininess);
+	vec3 specular=light.specular*spec*texture(material.specular,TexCor).rgb;
+
+	vec3 result=ambient+diffuse+specular;
+	color=vec4(result,1.0f);
 }
 
--- OpenGL/res/shaders/VertexShader.glsl
+++ OpenGL/res/shaders/VertexShader.glsl
@@ -1,14 +1,24 @@
 #version 330 core 
 layout (location=0) in vec3 positions;
-layout (location=1) in vec2 aTexCor;
+layout (location=1) in vec3 aNormal;
+layout (location=2) in vec2 aTextureCordinates;
 
+out vec3 Normal;
+out vec3 FragPos;
 out vec2 TexCor;
+
 uniform mat4 u_MVP;
+uniform mat4 u_model;
 
 void main()
 {
+	FragPos=vec3(u_model*vec4(positions,1.0f));
+
+	Normal=mat3(transpose(inverse(u_model)))*aNormal;
+
+	TexCor=aTextureCordinates;
+
 	gl_Position=u_MVP*vec4(positions,1.0f);
-	TexCor=aTexCor;
 }
 
 
