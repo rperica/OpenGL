abc40e5f054b2f63018183bb639c00e23afbea1a
rperica <zmaj@zmaj.com>
2020-03-09 20:08:52 +0100

    Created lighting

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -22,14 +22,9 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
 
-glm::mat4 LookAt(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
-glm::vec3 camPos = glm::vec3(0.0f, 0.0f, 1.0f);
-glm::vec3 camDir = glm::vec3(0.0f, 0.0f,-1.0f);
-glm::vec3 camUp  = glm::vec3(0.0f, 1.0f, 0.0f);
 
 bool firstMouse = true;
 float yaw =-90.0f;
@@ -41,7 +36,10 @@ float fov = 45.0f;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-Camera camera(glm::vec3(0.0f, 0.0f, 1.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 2.0f));
+
+glm::vec3 lightPos(1.2f,1.0f,2.0f);
+
 int main()
 {
     glfwInit();
@@ -68,32 +66,68 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
     {
-        float vertices[] = {
-            //vertices          //TextCor
-            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
-             0.5f,  0.5f, 0.0f, 1.0f, 1.0f,
-             0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f,
-        };
+       float vertices[] = {
+           -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+            0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+           -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+           -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
 
-        unsigned int indices[] = {
-            0,1,2,
-            0,3,1,
-        };
+           -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+           -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
+           -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
 
-        VertexArray va;
+           -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+           -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+           -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+           -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+           -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+           -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+            0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+            0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+           -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+            0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+           -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+           -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+
+           -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+           -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+           -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+        };
+      
         VertexBuffer vb(vertices, sizeof(vertices));
-        IndexBuffer ib(indices, 6);
+        
+        VertexArray va;
         va.AddBuffer(vb);
 
-        Shader shader("res/shaders/VertexShader.glsl", "res/shaders/FragmentShader.glsl");
+        VertexArray lightVa;
+        lightVa.AddBuffer(vb);
+
+        Shader shader("res/shaders/VertexShader.glsl","res/shaders/FragmentShader.glsl");
         shader.Bind();
 
-        Texture texture("res/textures/container.jpg");
-        texture.Bind();
+        Texture diffuseMap("res/textures/container2.png");
+        Texture specularMap("res/textures/container2_specular.png");
 
         Renderer render;
+
         while (!glfwWindowShouldClose(window))
         {
             float currentFrame = glfwGetTime();
@@ -101,15 +135,33 @@ int main()
             lastFrame = currentFrame;
 
             processInput(window);
-                 
-            glm::mat4 view = glm::mat4(1.0f);
-            view = camera.View();
+             
+            shader.Bind();
 
-            glm::mat4 mvp =view;
+            shader.SetUniformInt("material.diffuse", 0);
+            shader.SetUniformInt("material.specular", 1);
+
+            shader.SetUniformVec3("light.position", lightPos);
+            shader.SetUniformVec3("view", camera.GetPosition());
+        
+            shader.SetUniformFloat("material.shininess", 64.0f);
+
+            shader.SetUniformVec3("light.ambient", glm::vec3(0.2f));
+            shader.SetUniformVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+            shader.SetUniformVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
+            glm::mat4 projection = glm::perspective(glm::radians(camera.GetZoom()), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+            glm::mat4 view = camera.View();
+            glm::mat4 model = glm::mat4(1.0f);
+            glm::mat4 mvp = projection*view*model;
             shader.SetUniform4x4("u_MVP", mvp);
-            
+            shader.SetUniform4x4("u_model",model);
+
+            diffuseMap.Bind(0);
+            specularMap.Bind(1);
+
             render.Clear();
-            render.Draw(va, ib, shader);
+            render.Draw(va,shader,36);
 
             glfwSwapBuffers(window);
             glfwPollEvents();
@@ -124,7 +176,6 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         camera.KeyboardInput(FORWARD, deltaTime);
@@ -161,5 +212,5 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-    camera.ScrollProcess();
+    camera.ScrollProcess(yoffset);
 }
